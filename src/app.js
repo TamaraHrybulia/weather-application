ba967d0343a564417845c635d52c5c268783bf26
TamaraHrybulia <tamara.grybulia@gmail.com>
2022-12-08 15:53:42 +0200

    added forecast display in HTML and JS

--- src/app.js
+++ src/app.js
@@ -112,6 +112,62 @@ function showWeatherData(response) {
   );
   mainIconElement.setAttribute("alt", response.data.condition.description);
   mainIconElement.style.width = "150px";
+
+  getForecast(response.data.coordinates);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "30f3621d44ta8b790e7433b8ef5bf7ao";
+  let apiEndpoint = "https://api.shecodes.io/weather/v1/forecast";
+  let units = "metric";
+  let lat = coordinates.latitude;
+  let lon = coordinates.longitude;
+  let apiUrl = `${apiEndpoint}?lon=${lon}&lat=${lat}&units=${units}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecast = response.data.daily;
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6 && index > 0) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+        <div class="col mt-5 mb-5 w-100">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">${formatForecastDay(
+                  forecastDay.time
+                )}</h4>
+                <p class="card-image"> <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                  forecastDay.condition.icon
+                }.png" alt=""  /> </p>
+                <p class="card-day-temperature"> ${Math.round(
+                  forecastDay.temperature.maximum
+                )}°</p>
+                <p class="card-night-temperature">${Math.round(
+                  forecastDay.temperature.minimum
+                )}°</p>
+              </div>
+            </div>
+          </div>
+          `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
 }
 
 function displayCelsiusTemperature(event) {
